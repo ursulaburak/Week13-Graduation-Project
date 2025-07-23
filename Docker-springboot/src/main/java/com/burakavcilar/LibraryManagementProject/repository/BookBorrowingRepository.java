e42a1b5da51671e8f634478d56b9a0154ca23b1a
Burak Avcılar <burakkavcilar@gmail.com>
2025-07-23 13:41:44 +0300

    Delete Docker-springboot directory

--- Docker-springboot/src/main/java/com/burakavcilar/LibraryManagementProject/repository/BookBorrowingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.burakavcilar.LibraryManagementProject.repository;
-
-import com.burakavcilar.LibraryManagementProject.entity.BookBorrowing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface  BookBorrowingRepository extends JpaRepository<BookBorrowing,Long> {
-}
